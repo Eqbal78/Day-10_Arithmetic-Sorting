2af577d7482fd44ad9b242d243abf4ef97923186
Eqbal78 <ahmadiqbal.a786@gmail.com>
2021-08-22 20:22:38 +0530

    Sorting in Ascending order

---
+++ UC-9_SortAscendingOrder.sh
@@ -0,0 +1,59 @@
+#! /bin/bash 
+
+read -p:"Enter First Element: " a
+read -p:"Enter Second Element: " b
+read -p:"Enter Third Element: " c
+
+declare -A result
+arry=()
+
+function operation(){
+
+   result[0]=$(($a+$b*$c))
+   result[1]=$(( ($a+$b) * $c))
+
+	result[2]=$(($a*$b+$c))
+   result[3]=$(($a*($b+$c)))
+
+   result[4]=$(($c+$a/$b))
+   result[5]=$((($c+$a) / $b))
+
+	result[6]=$(($a%$b+$c))
+   result[7]=$(($a % ($b+$c)))
+}
+operation $a $b $c
+
+echo "Add-Multiplication"
+echo "Result=${result[0]}"
+echo -e "Result01=${result[1]}\n"
+
+echo "Multiplication-Add"
+echo "Result02=${result[2]}"
+echo -e "Result03=${result[3]}\n"
+
+echo "Add-Division"
+echo "Result04=${result[4]}"
+echo -e "Result05=${result[5]}\n"
+
+echo "Module-Add"
+echo "Result06=${result[6]}"
+echo "Result07=${result[7]}"
+
+echo ${result[@]}
+
+len=${#result[@]}
+
+for (( i=0; i<=$len; i++))
+do
+	for ((j=$i; j<=$len; j++))
+	do
+		if [[ ${result[$i]} -gt ${result[$j]} ]]
+		then
+			t=${result[$j]}
+			result[$j]=${result[$i]}
+			result[$i]=$t
+		fi
+	done
+done
+
+echo "Result in Asecending order: ${result[@]}"
